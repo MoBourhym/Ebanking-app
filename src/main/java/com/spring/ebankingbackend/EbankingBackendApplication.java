cfc684bf8ce613a03af523fa608d38422b0c346c
MoBourhym <simobourhym01@gmail.com>
2025-05-17 15:15:04 +0100

    first commit

---
+++ src/main/java/com/spring/ebankingbackend/EbankingBackendApplication.java
@@ -0,0 +1,14 @@
+package com.spring.ebankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBackendApplication.class, args);
+    }
+
+
+}
